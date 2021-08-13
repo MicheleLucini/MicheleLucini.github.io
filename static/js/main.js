73bf029327ee56baf8146f7207744799bfcab056
Lucini Michele <mic.lucini@gmail.com>
2021-08-14 01:42:59 +0200

    Big changes

--- static/js/main.js
+++ static/js/main.js
@@ -1,80 +1,47 @@
+var $hours;
+var $minutes;
+var $seconds;
 var $content;
-var $porta;
-var $stella;
-var viewport = {
-  height: 0,
-  width: 0,
-};
 
 (function () {
+  clockInitialize();
+
   $content = document.getElementById("content");
-  $porta = document.getElementById("porta");
-  $stella = document.getElementById("stella");
-
-  updateViewportMeasurements();
   setContentMinHeight();
-
   window.addEventListener("resize", handleResize);
-  document.onmousemove = handleMouseMove;
-  document.onmouseenter = handleMouseEnter;
-  document.onmouseleave = handleMouseLeave;
 })();
 
-function updateViewportMeasurements() {
-  viewport = {
-    height: window.innerHeight,
-    width: window.innerWidth,
-  };
+function clockInitialize() {
+  $hours = document.getElementById("hours");
+  $minutes = document.getElementById("minutes");
+  $seconds = document.getElementById("seconds");
+
+  clockUpdate();
+
+  setTimeout(clockUpdate, 100);
+}
+
+function clockUpdate() {
+  let today = new Date();
+  let s = today.getSeconds(); // 0 - 59
+  let m = today.getMinutes(); // 0 - 59
+  let h = (today.getHours() % 12) + m / 59; // 22 % 12 = 10pm
+
+  s *= 6; // 60 * 6 = 360deg
+  m *= 6;
+  h *= 30; // 12 * 30 = 360deg
+
+  $seconds.style.transform = `rotate(${s}deg)`;
+  $minutes.style.transform = `rotate(${m}deg)`;
+  $hours.style.transform = `rotate(${h}deg)`;
+
+  setTimeout(clockUpdate, 500);
 }
 
 function setContentMinHeight() {
-  $content.style.minHeight = viewport.height - 100 + "px";
+  $content.style.minHeight = window.innerHeight - 100 + "px";
 }
 
 function handleResize() {
-  updateViewportMeasurements();
-  resetElementsPosition();
   setContentMinHeight();
-}
-
-function handleMouseMove(event) {
-  const e = event || window.event;
-  const { x, y } = cursorPositionPx2Pct(e.x, e.y, viewport);
-  changeElementsPosition(x, y);
-}
-
-function handleMouseEnter() {
-  resetElementsPosition();
-}
-
-function handleMouseLeave() {
-  resetElementsPosition();
-}
-
-function cursorPositionPx2Pct(x, y, viewport) {
-  return {
-    x: (x * 100) / viewport.width,
-    y: (y * 100) / viewport.height,
-  };
-}
-
-function resetElementsPosition() {
-  changeElementsPosition(50, 50);
-}
-
-function changeElementsPosition(xPercentage, yPercentage) {
-  var xVariationPx = (xPercentage - 50) / 2;
-  var yVariationPx = (yPercentage - 50) / 2;
-
-  // default transform: translateX(-200px) translateY(120px);
-  $porta.style.transform = getNewStyle(-200 + xVariationPx, 120 + yVariationPx);
-  // default transform: translateX(180px) translateY(-300px);
-  $stella.style.transform = getNewStyle(
-    180 + xVariationPx,
-    -300 + yVariationPx
-  );
-}
-
-function getNewStyle(x, y) {
-  return "translateX(" + x + "px) translateY(" + y + "px)";
 }
