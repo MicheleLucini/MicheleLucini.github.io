0b73d057257fc2764a00f1a77c637d4778bf1f34
Michele Lucini <mic.lucini@gmail.com>
2023-07-11 19:43:52 +0200

    Links style

--- main.js
+++ main.js
@@ -1,47 +1,47 @@
-var $hours;
-var $minutes;
-var $seconds;
-var $content;
+// var $hours;
+// var $minutes;
+// var $seconds;
+// var $content;
 
-(function () {
-  clockInitialize();
+// (function () {
+//   clockInitialize();
 
-  $content = document.getElementById("content");
-  setContentMinHeight();
-  window.addEventListener("resize", handleResize);
-})();
+//   $content = document.getElementById("content");
+//   setContentMinHeight();
+//   window.addEventListener("resize", handleResize);
+// })();
 
-function clockInitialize() {
-  $hours = document.getElementById("hours");
-  $minutes = document.getElementById("minutes");
-  $seconds = document.getElementById("seconds");
+// function clockInitialize() {
+//   $hours = document.getElementById("hours");
+//   $minutes = document.getElementById("minutes");
+//   $seconds = document.getElementById("seconds");
 
-  clockUpdate();
+//   clockUpdate();
 
-  setTimeout(clockUpdate, 100);
-}
+//   setTimeout(clockUpdate, 100);
+// }
 
-function clockUpdate() {
-  let today = new Date();
-  let s = today.getSeconds(); // 0 - 59
-  let m = today.getMinutes(); // 0 - 59
-  let h = (today.getHours() % 12) + m / 59; // 22 % 12 = 10pm
+// function clockUpdate() {
+//   let today = new Date();
+//   let s = today.getSeconds(); // 0 - 59
+//   let m = today.getMinutes(); // 0 - 59
+//   let h = (today.getHours() % 12) + m / 59; // 22 % 12 = 10pm
 
-  s *= 6; // 60 * 6 = 360deg
-  m *= 6;
-  h *= 30; // 12 * 30 = 360deg
+//   s *= 6; // 60 * 6 = 360deg
+//   m *= 6;
+//   h *= 30; // 12 * 30 = 360deg
 
-  $seconds.style.transform = `rotate(${s}deg)`;
-  $minutes.style.transform = `rotate(${m}deg)`;
-  $hours.style.transform = `rotate(${h}deg)`;
+//   $seconds.style.transform = `rotate(${s}deg)`;
+//   $minutes.style.transform = `rotate(${m}deg)`;
+//   $hours.style.transform = `rotate(${h}deg)`;
 
-  setTimeout(clockUpdate, 500);
-}
+//   setTimeout(clockUpdate, 500);
+// }
 
-function setContentMinHeight() {
-  $content.style.minHeight = window.innerHeight - 100 + "px";
-}
+// function setContentMinHeight() {
+//   $content.style.minHeight = window.innerHeight - 100 + "px";
+// }
 
-function handleResize() {
-  setContentMinHeight();
-}
+// function handleResize() {
+//   setContentMinHeight();
+// }
